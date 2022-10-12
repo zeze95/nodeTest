be8e4b9ddbf492bd893edbd9e728147530710ffb
zezaek <kejae214@gmail.com>
2022-10-12 21:45:56 +0900

    1012

--- hello/01-token/index.js
+++ hello/01-token/index.js
@@ -1,1 +1,19 @@
-console.log("start");
+function getToken(count) {
+  const result = String(Math.floor(Math.random() * 10 ** count)).padStart(
+    count,
+    "0"
+  );
+  if (count >= 11) {
+    console.log("너무 많은 자리수입니다.");
+  } else if (count <= 4) {
+    console.log("너무 적은 자리수입니다.");
+  } else {
+    console.log(result);
+  }
+
+  return result;
+}
+
+getToken(7);
+getToken(3);
+getToken(12);
