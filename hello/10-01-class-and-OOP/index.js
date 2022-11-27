a1eb37063499261bb07327d48faf9a5d51e1391a
zezaek <kejae214@gmail.com>
2022-11-27 21:09:34 +0900

    oop

---
+++ hello/10-01-class-and-OOP/index.js
@@ -0,0 +1,28 @@
+const aaa = new Date()
+console.log(aaa.getFullYear())
+console.log(aaa.getMonth() + 1)
+
+class Monster {
+    power = 10
+
+    constructor(aaa){
+        this.power = aaa
+    }
+
+    attack = () => {
+        console.log("공격하자!!")
+        console.log("내 공격력은 " + this.power + " 이야!!!")
+    }
+
+    run = () => {
+        console.log("도망가자!!")
+    }
+}
+
+const mymonster1 = new Monster(10)
+mymonster1.attack()
+mymonster1.run()
+
+const mymonster2 = new Monster(50)
+mymonster2.attack()
+mymonster2.run()
